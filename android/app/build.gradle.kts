1a6e9f6b523b9784296596e0795f97d6c1e5ace5
Yılmaz <yilmazsannli@gmail.com>
2026-08-31 17:01:48 +0300

    Fix compile error SDK Mismatch

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.accessibility_service"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
